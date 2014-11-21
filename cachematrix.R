4a80e71ba98ea4d36688df867ec44812125fe691
Mateus Florizi <mflorizi@hotmail.com>
2014-11-21 21:59:49 -0200

    Commit Programming Assignment2: Caching Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program gets a matrix, inverts it and return an inverted matrix.
+## It caches the resulting matrix to avoid it recalculates again in order to save computation time
+## It takes advantage of lexical scoping in order to avoid rerunning the calculation if matrix
+## has not changed since last run
 
-## Write a short comment describing this function
+## This funciton takes the matrix and return a list with the necessary methods
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  iv <- NULL
+  set <- function(y) {
+    x <<- y
+    iv <<- NULL
+  }
+  get <- function() x
+  setInvMatrix <- function(InvMatrix) iv <<- InvMatrix
+  getInvMatrix <- function() iv
+  list(set = set, get = get,
+       setInvMatrix = setInvMatrix,
+       getInvMatrix = getInvMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function uses the methods created in the makeCacheMatrix and calculate the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  iv <- x$getInvMatrix()
+  if(!is.null(iv)) {
+    message("getting cached data")
+    return(iv)
+  }
+  data <- x$get()
+  iv <- solve(data, ...)
+  x$setInvMatrix(iv)
+  iv
 }
